776914b2c39a70af25f7058048d0cad78c39abce
Lauren-Nicole <popich.red@gmail.com>
2022-11-09 15:35:05 +0000

    Apply loose styles to header component.

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,14 +1,62 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 import SiteLinks from './SiteLinks'
 
 const Navbar = () => {
+    const styles = {
+        main: {
+            border:"1px solid #D3D3D3",
+            borderRadius: "20px",
+            margin: "20px",
+            width: "100%",
+            alignItems: "center",
+            padding: "20px",
+            // boxSizing: "border-box",
+        },
+        link: {
+            padding: "0",
+        },
+    };
+
     return (
-        <div>
-            <SiteLinks title="Home" urlpath=""/>
-            <SiteLinks title="Chat" urlpath=""/>
-            <SiteLinks title="Employees" urlpath=""/>
-            <SiteLinks title="Settings" urlpath=""/>
-        </div>
+        <Grid
+            container
+            xs={12}
+            spacing={2}
+            direction="row"
+            justifyContent="space-between"
+            style={styles.main}
+        >
+            <Grid
+                item
+                xs={3}
+                style={styles.link}
+            >
+                <SiteLinks title="Home" urlpath="/"/>
+            </Grid>
+            
+            <Grid
+                item
+                xs={3}
+                style={styles.link}
+            >
+                <SiteLinks title="Chat" urlpath="/chat"/>
+            </Grid>
+            <Grid
+                item
+                xs={3}
+                style={styles.link}
+            >
+                <SiteLinks title="Employees" urlpath="/employees"/>
+            </Grid>
+            <Grid
+                item
+                xs={3}
+                style={styles.link}
+            >
+                <SiteLinks title="Settings" urlpath="/settings"/>
+            </Grid>
+        </Grid>
     )
 }
 
