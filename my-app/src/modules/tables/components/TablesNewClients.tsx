3915b90e3cf3a58f04920d6a3e35da28b2abf961
Lauren-Nicole <popich.red@gmail.com>
2022-11-10 12:47:28 +0000

    import tables into home. implement grid.

---
+++ my-app/src/modules/tables/components/TablesNewClients.tsx
@@ -0,0 +1,16 @@
+import Tables from './Tables'
+
+const TablesNewClients = () => {
+    return (
+        <Tables
+            tablehead="New Clients"
+            columnhead1="Avatar"    
+            columnhead2="First Name"
+            columnhead3="Last Name"
+            columnhead4="Location"
+            columnhead5="Age"
+        />
+    )
+}
+
+export default TablesNewClients
--- my-app/src/pages/HomePage.tsx
+++ my-app/src/pages/HomePage.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { Grid } from '@mui/material'
 import TablesEmployees from '../modules/tables/components/TablesEmployees'
+import TablesNewClients from '../modules/tables/components/TablesNewClients'
 
 const HomePage = () => {
     return (
         <div>
-            <TablesEmployees />
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <TablesEmployees />
+                </Grid>
+                <Grid item xs={6}>
+                    <TablesNewClients />
+                </Grid>
+            </Grid>
         </div>
     )
 }
