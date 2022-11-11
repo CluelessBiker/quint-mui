aad968b0bd1f8b7fadd83f6783cbc7020e55154c
Lauren-Nicole <popich.red@gmail.com>
2022-11-11 09:56:34 +0000

    Create container for modals.

---
+++ my-app/src/modules/modals/components/ModalContainer.tsx
@@ -0,0 +1,30 @@
+import { Container, Typography } from '@mui/material';
+import { FC } from 'react';
+import SiteButtons from '../../common/components/SiteButtons';
+
+interface ModalContainerProps {
+    title: string,
+    bodytext?: string,
+    bgcolor?: string,
+}
+
+const ModalContainer:FC<ModalContainerProps> = ({ title, bodytext, bgcolor }) => {
+    return (
+        <Container
+            sx={{
+                backgroundColor: `#${ bgcolor }`,
+                borderRadius: '20px',
+                border:'1px solid #333333',
+            }}
+        >
+            <Typography variant="h6" gutterBottom>{ title }</Typography>
+            <Typography variant="body2" gutterBottom>{ bodytext }</Typography>
+
+            <SiteButtons name="Cancel" path="/" bgcolor="FC9595"/>
+            <SiteButtons name="Save" path="/" bgcolor="87DAC1"/>
+
+        </Container>
+    )
+}
+
+export default ModalContainer
