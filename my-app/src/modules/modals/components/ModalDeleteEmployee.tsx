7ac468b58fedc31359d047477fc4ef8e64935fe6
Lauren-Nicole <popich.red@gmail.com>
2022-11-11 12:00:02 +0000

    Create component for delte-employee modal.

---
+++ my-app/src/modules/modals/components/ModalDeleteEmployee.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ModalContainer from './ModalContainer'
+
+const ModalDeleteEmployee = () => {
+    return (
+        <ModalContainer title="Delete Employee" bodytext="User {username} will be deleted" showhide="block">
+            <h1>TEST</h1>
+        </ModalContainer>
+    )
+}
+
+export default ModalDeleteEmployee
