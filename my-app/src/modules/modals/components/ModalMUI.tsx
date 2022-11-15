cff83551531925c16db11c5545b8f2267172643b
Lauren-Nicole <popich.red@gmail.com>
2022-11-15 14:35:30 +0000

    Adjust styling of MUI modal to match old version.

--- my-app/src/modules/modals/components/ModalMUI.tsx
+++ my-app/src/modules/modals/components/ModalMUI.tsx
@@ -1,7 +1,5 @@
-// import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { FC, useState } from 'react';
 
@@ -12,7 +10,8 @@ const style = {
     transform: 'translate(-50%, -50%)',
     width: 400,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
+    borderRadius: '20px',
+    border:'1px solid #333333',
     boxShadow: 24,
     p: 4,
 };
@@ -50,13 +49,7 @@ const ModalMUI:FC<ModalMUIProps> = ({ bgcolor, btnname, children }) => {
             aria-describedby="modal-modal-description"
         >
             <Box sx={style}>
-            {/* <Typography id="modal-modal-title" variant="h6" component="h2">
-                Text in a modal
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography> */}
-            { children }
+                { children }
             </Box>
         </Modal>
         </div>
