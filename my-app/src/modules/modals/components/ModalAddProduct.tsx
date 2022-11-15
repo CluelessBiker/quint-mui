8bc075114082cb74e8892d45d03c123b366cd1af
Lauren-Nicole <popich.red@gmail.com>
2022-11-15 14:43:55 +0000

    Create modal for Add products.

--- my-app/src/modules/modals/components/ModalAddProduct.tsx
+++ my-app/src/modules/modals/components/ModalAddProduct.tsx
@@ -1,9 +1,31 @@
+import { Typography } from '@mui/material'
+import FormField from '../../common/components/FormField'
+import SiteButtons from '../../common/components/SiteButtons'
 import ModalMUI from './ModalMUI'
 
 const ModalAddProduct = () => {
     return (
         <ModalMUI bgcolor="87DAC1" btnname="+ New Employee">
-            
+            <Typography
+                variant="h6"
+                gutterBottom
+                sx={{marginBottom: '20px'}}
+            >
+                Add Product
+            </Typography>
+
+            <FormField label="title"/>
+            <FormField label="description"/>
+            <FormField label="price"/>
+            <FormField label="discountPercentage"/>
+            <FormField label="rating"/>
+            <FormField label="stock"/>
+            <FormField label="brand"/>
+            <FormField label="category"/>
+            <FormField label="thumbnail"/>
+
+            <SiteButtons name="Cancel" path="/" bgcolor="FC9595"/>
+            <SiteButtons name="Save" path="/" bgcolor="87DAC1"/>
         </ModalMUI>
     )
 }
