13ce8b45457c6ac7941059fca89a0195c6541453
Lauren-Nicole <popich.red@gmail.com>
2022-11-15 13:37:25 +0000

    remove commented out code from ProductsTable

--- my-app/src/modules/tables/components/ProductsTable.tsx
+++ my-app/src/modules/tables/components/ProductsTable.tsx
@@ -7,42 +7,6 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { initialState, Product } from '../../../redux/productsSlice';
 
-// function createData(
-//     title: string,
-//     description: string,
-//     price: number,
-//     discountPercentage: number,
-//     rating: number,
-//     stock: number,
-//     brand: string,
-//     category: string,
-//     thumbnail: string,
-//     images: string[],
-// ) {
-//     return { 
-//         title,
-//         description,
-//         price,
-//         discountPercentage,
-//         rating,
-//         stock,
-//         brand,
-//         category,
-//         thumbnail,
-//         images,
-//     };
-// }
-
-// const rows = [
-//     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//     createData('Eclair', 262, 16.0, 24, 6.0),
-//     createData('Cupcake', 305, 3.7, 67, 4.3),
-//     createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
-
-
-
 const ProductsTable = () => {
     const rows = initialState.products;
     return (
