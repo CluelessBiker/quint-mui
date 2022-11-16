7ec9c5e21ff7394e1a9e2c917036e7f94c0f5157
Lauren-Nicole <popich.red@gmail.com>
2022-11-16 13:00:19 +0000

    Comment out test code in employees page.

--- my-app/src/pages/EmployeesPage.tsx
+++ my-app/src/pages/EmployeesPage.tsx
@@ -1,5 +1,5 @@
 import { Container, Grid, Typography } from '@mui/material'
-import { useDispatch } from 'react-redux'
+// import { useDispatch } from 'react-redux'
 import HRLine from '../modules/common/components/HRLine'
 import ModalAddProduct from '../modules/modals/components/ModalAddProduct'
 import ProductsTable from '../modules/tables/components/ProductsTable'
